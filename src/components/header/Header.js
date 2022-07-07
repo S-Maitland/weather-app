b3a47ed2bfcd3938a4b59fb72c53e60cc39091c3
George Ciurescu <george.ciurescu@verint.com>
2022-07-07 21:42:00 +0100

    Basic structure created

---
+++ src/components/header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.scss';
+
+// const Header = () => { 
+//     return (
+//         <div>
+//             React Weather App
+//         </div>
+//     )
+// }
+   
+
+// export default Header;
+
+export default function Header() {
+    return (
+        <div>
+           <p>React Weather App 2</p> 
+        </div>
+    )
+}
+
+
+// export default function ReactApp() {
+//     return <p>Exported Using Default Export</p>
+//   }
