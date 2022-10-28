3171ced3d90b5f50da6ab99b5199d54481caf4f9
George Ciurescu <george.ciurescu@verint.com>
2022-10-28 20:16:39 +0100

    Minor css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,9 @@ import './Header.scss';
 
 export default function Header() {
     return (
-        <>
-            <h2>Weather Fetcher</h2>
-        </>
+        <div className="header">
+    
+            <h1>Weather App</h1>
+        </div>
     )
 }
