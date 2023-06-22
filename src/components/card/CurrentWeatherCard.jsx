241fca2981f0b5427b71f3896052aa750801bdcf
onitwin <onitwin@gmail.com>
2023-06-22 22:07:19 +0100

    styled applied to current weather box

--- src/components/card/CurrentWeatherCard.jsx
+++ src/components/card/CurrentWeatherCard.jsx
@@ -4,6 +4,10 @@ const CurrentWeatherCard = ({ currentWeather }) => {
 
     return (
         <div className="cardContainer">
+            <div className="cardTitle">
+                Current Weather
+            </div>
+            <div className="currentWeatherGroup">
             <div className="iconWrap">
                 <img src={`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`} alt="weatherIcon" />
                 {currentWeather.weather[0].description}
@@ -60,6 +64,7 @@ const CurrentWeatherCard = ({ currentWeather }) => {
                     <div className='dataValue'>
                         <p>{`${currentWeather.main.humidity}%`}</p>
                     </div>
+                </div>
                 </div>
                 </div>
         </div>
